625a615e4fe7c68550d14eb42174a6e1a8b31e96
haidarali751 <haidaralishaikh751@gmail.com>
2025-06-27 12:12:00 +0530

    feat: allow multiple custom delimiters of any length

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -13,19 +13,21 @@ export function add(input) {
         delimiterLine
           .slice(1, -1)
           .split("][")
-          .map((d) => `(${d})`)
+          .map((d) => d.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&"))
           .join("|")
       );
     } else {
-      delimiterPattern = new RegExp(`[${delimiterLine}]`);
+      delimiterPattern = new RegExp(
+        `[${delimiterLine.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&")}]`
+      );
     }
 
     numbersPart = input.slice(delimiterSectionEnd + 1);
   }
 
   const numbers = numbersPart.split(delimiterPattern).map(Number);
-  const negativeNumbers = numbers.filter((num) => num < 0);
 
+  const negativeNumbers = numbers.filter((num) => num < 0);
   if (negativeNumbers.length > 0) {
     throw new Error(
       `Negative numbers not allowed: ${negativeNumbers.join(", ")}`
--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -1,11 +1,11 @@
-import { add } from './stringCalculator';
+import { add } from "./stringCalculator";
 
-describe('String Calculator', () => {
-  test('should return 0 for empty string', () => {
+describe("String Calculator", () => {
+  test("should return 0 for empty string", () => {
     expect(add("")).toBe(0);
   });
 
-  test('should return number for a single number', () => {
+  test("should return number for a single number", () => {
     expect(add("1")).toBe(1);
   });
 
@@ -25,17 +25,22 @@ describe('String Calculator', () => {
     expect(add("//;\n1;2")).toBe(3);
   });
 
-   test("should throw an error on negative numbers", () => {
-     expect(() => add("1,-2,3,-5")).toThrow(
-       "Negative numbers not allowed: -2, -5"
-     );
-   });
+  test("should throw an error on negative numbers", () => {
+    expect(() => add("1,-2,3,-5")).toThrow(
+      "Negative numbers not allowed: -2, -5"
+    );
+  });
 
-    test("should ignore numbers greater than 1000", () => {
-      expect(add("2,1001")).toBe(2);
-    });
+  test("should ignore numbers greater than 1000", () => {
+    expect(add("2,1001")).toBe(2);
+  });
 
-     test("should support delimiters of any length", () => {
-       expect(add("//[***]\n1***2***3")).toBe(6);
-     });
+  test("should support delimiters of any length", () => {
+    expect(add("//[***]\n1***2***3")).toBe(6);
+  });
+
+  test("should support multiple custom delimiters of any length", () => {
+    expect(add("//[*][%]\n1*2%3")).toBe(6);
+    expect(add("//[**][%%]\n1**2%%3")).toBe(6);
+  });
 });
