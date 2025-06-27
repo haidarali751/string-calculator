2d759a25f2bd3ba79fe183ec6c6bd9d5959cae64
haidarali751 <haidaralishaikh751@gmail.com>
2025-06-27 11:58:01 +0530

    feat: add support for custom delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,16 @@
 export function add(input) {
   if (!input) return 0;
-  const numbers = input.split(/,|\n/).map(Number);
+
+  let delimiterPattern = /,|\n/;
+  let numbersPart = input;
+
+  if (input.startsWith("//")) {
+    const delimiterSectionEnd = input.indexOf("\n");
+    const customDelimiter = input.slice(2, delimiterSectionEnd);
+    delimiterPattern = new RegExp(`[${customDelimiter}]`);
+    numbersPart = input.slice(delimiterSectionEnd + 1);
+  }
+
+  const numbers = numbersPart.split(delimiterPattern).map(Number);
   return numbers.reduce((sum, num) => sum + num, 0);
 }
--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -20,4 +20,8 @@ describe('String Calculator', () => {
   test("should support new lines as delimiters", () => {
     expect(add("1\n2,3")).toBe(6);
   });
+
+  test("should support custom delimiters", () => {
+    expect(add("//;\n1;2")).toBe(3);
+  });
 });
